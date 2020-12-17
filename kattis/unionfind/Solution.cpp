854fc77549fdce60ca16ead34e6cc7628862d4db
raysonkoh <rayson.rk.koh@gmail.com>
2020-12-17 22:03:01 +0800

    Add ufds sols and add template cc

--- kattis/unionfind/Solution.cpp
+++ kattis/unionfind/Solution.cpp
@@ -1,26 +1,74 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-class ufds {
+class UnionFind {
 public:
-  void print() { cout << "hello world!" << endl; }
-};
+  int parent[1000000];
+  int rank[1000000];
 
-int main() {
-  long long n, q;
-  for (long long i = 0; i < q; i++) {
-    char c;
-    long long a, b;
-    cin >> c >> a >> b;
-    if (c == '?') {
-      // query
-    } else {
-      // merge
+  UnionFind(int n) {
+    for (int i = 0; i < n; i++) {
+      parent[i] = i;
+      rank[i] = 0;
     }
   }
 
-  ufds u;
-  u.print();
+  int findCompId(int x) {
+    while (parent[x] != x) {
+      x = parent[x];
+    }
+    return x;
+  }
+
+  bool isSameSet(int p, int q) { return findCompId(p) == findCompId(q); }
+
+  void unionSet(int p, int q) {
+    int pid = findCompId(p);
+    int qid = findCompId(q);
+    if (pid == qid) {
+      return;
+    }
+    if (rank[pid] > rank[qid]) {
+      swap(pid, qid);
+    }
+    parent[pid] = qid;
+    if (rank[pid] == rank[qid]) {
+      rank[qid]++;
+    }
+  }
+};
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  int n, q;
+  cin >> n >> q;
+  UnionFind ufds(n);
+
+  string s;
+  int a, b;
+
+  for (int i = 0; i < q; i++) {
+    cin >> s >> a >> b;
+    if (s == "=") {
+      // union
+      ufds.unionSet(a, b);
+    } else {
+      // find
+      bool res = ufds.isSameSet(a, b);
+      if (res) {
+        cout << "yes"
+             << "\n";
+      } else {
+        cout << "no"
+             << "\n";
+      }
+    }
+  }
+
   return 0;
 }
