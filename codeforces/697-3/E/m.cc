597e44d3462224677a766d7c47a2afdc3c4808a1
raysonkoh <rayson.rk.koh@gmail.com>
2021-01-31 17:55:53 +0800

    Update nCk template to use fact instead of dp

--- codeforces/697-3/E/m.cc
+++ codeforces/697-3/E/m.cc
@@ -19,36 +19,56 @@ const long long P = 1e9 + 7;
 const double EPS = 1e-11;
 
 using namespace std;
+
 class NChooseK {
-public:
-  vector<vector<long long>> binomTable;
-  NChooseK(int nMax, int kMax) {
-    binomTable.assign(nMax + 1, vector<long long>(kMax + 1));
-    initBinomTable();
+ public:
+  vector<long long> fact;
+  vector<long long> factInv;
+
+  NChooseK(int n) {
+    fact.assign(n + 1, 1);
+    factInv.assign(n + 1, 1);
+    init();
   }
-  void initBinomTable() {
-    for (long long i = 0; i < binomTable.size(); i++) {
-      for (long long j = 0; j < binomTable[i].size(); j++) {
-        if (j == 0) {
-          binomTable[i][j] = 1;
-        } else if (i == 0) {
-          binomTable[i][j] = 0;
-        } else if (j == 1) {
-          binomTable[i][j] = i;
-        } else {
-          binomTable[i][j] = (binomTable[i - 1][j] + binomTable[i - 1][j - 1]) % P;
-        }
-      }
+  void init() {
+    for (int i = 1; i < fact.size(); i++) {
+      fact[i] = (fact[i - 1] * i) % P;
+      factInv[i] = invMod(fact[i]);
     }
   }
-  long long query(int n, int k) { return binomTable[n][k]; }
+  long long invMod(long long x) {
+    // Fermat Little Thm: a^p = a (mod p), p is prime
+    // thus, a^{p - 1} = 1 (mod p)
+    // --> a * invMod(a) = 1 = a^{p - 1} (mod p)
+    // --> invMod(a) = a^{p - 2} (mod p)
+    return fastMod(x, P - 2);
+  }
+  long long fastMod(long long a, long long exp) {
+    if (exp == 0) {
+      return 1;
+    } else if (exp == 1) {
+      return a % P;
+    }
+
+    long long mid = exp / 2;
+    long long tmp = fastMod(a, mid);
+    if (exp % 2 == 0) {
+      return (tmp * tmp) % P;
+    } else {
+      return ((tmp * tmp) % P * a) % P;
+    }
+  }
+  long long query(int n, int k) {
+    // nCk = n! / (k! * (n - k)!)
+    return (((fact[n] * factInv[k]) % P) * factInv[n - k]) % P;
+  }
 };
 
 int main() {
   ios_base::sync_with_stdio(false);
   cin.tie(NULL);
   cout.tie(NULL);
-  NChooseK nck(1000, 1000);
+  NChooseK nck(2000);
   int t;
   cin >> t;
   while (t--) {
--- ownlib/algos/n_choose_k.cc
+++ ownlib/algos/n_choose_k.cc
@@ -3,10 +3,56 @@
 
 using namespace std;
 
+const long long P = 1e9 + 7;
+
 class NChooseK {
-public:
+ public:
+  vector<long long> fact;
+  vector<long long> factInv;
+
+  NChooseK(int n) {
+    fact.assign(n + 1, 1);
+    factInv.assign(n + 1, 1);
+    init();
+  }
+  void init() {
+    for (int i = 1; i < fact.size(); i++) {
+      fact[i] = (fact[i - 1] * i) % P;
+      factInv[i] = invMod(fact[i]);
+    }
+  }
+  long long invMod(long long x) {
+    // Fermat Little Thm: a^p = a (mod p), p is prime
+    // thus, a^{p - 1} = 1 (mod p)
+    // --> a * invMod(a) = 1 = a^{p - 1} (mod p)
+    // --> invMod(a) = a^{p - 2} (mod p)
+    return fastMod(x, P - 2);
+  }
+  long long fastMod(long long a, long long exp) {
+    if (exp == 0) {
+      return 1;
+    } else if (exp == 1) {
+      return a % P;
+    }
+
+    long long mid = exp / 2;
+    long long tmp = fastMod(a, mid);
+    if (exp % 2 == 0) {
+      return (tmp * tmp) % P;
+    } else {
+      return ((tmp * tmp) % P * a) % P;
+    }
+  }
+  long long query(int n, int k) {
+    // nCk = n! / (k! * (n - k)!)
+    return (((fact[n] * factInv[k]) % P) * factInv[n - k]) % P;
+  }
+};
+
+class NChooseKTable {
+ public:
   vector<vector<long long>> binomTable;
-  NChooseK(int nMax, int kMax) {
+  NChooseKTable(int nMax, int kMax) {
     binomTable.assign(nMax + 1, vector<long long>(kMax + 1));
     initBinomTable();
   }
@@ -18,9 +64,9 @@ public:
         } else if (i == 0) {
           binomTable[i][j] = 0;
         } else if (j == 1) {
-          binomTable[i][j] = i;
+          binomTable[i][j] = i % P;
         } else {
-          binomTable[i][j] = binomTable[i - 1][j] + binomTable[i - 1][j - 1];
+          binomTable[i][j] = (binomTable[i - 1][j] + binomTable[i - 1][j - 1]) % P;
         }
       }
     }
@@ -29,10 +75,14 @@ public:
 };
 
 int main() {
-  NChooseK nChooseK(1e5, 1e3);
-  cout << nChooseK.query(5, 2) << endl;  // 10
-  cout << nChooseK.query(3, 2) << endl;  // 3
-  cout << nChooseK.query(10, 1) << endl; // 10
-  cout << nChooseK.query(0, 1) << endl;  // 0
-  cout << nChooseK.query(2, 0) << endl;  // 1
+  NChooseK nCk(1000);
+  NChooseKTable brute(1000, 1000);
+  for (int n = 0; n < 1000; n++) {
+    for (int k = n - 1; k >= 0; k--) {
+      long long res = nCk.query(n, k);
+      long long expect = brute.query(n, k);
+      if (res != expect)
+        cout << "n: " << n << " k: " << k << " res: " << res << " expect: " << expect << endl;
+    }
+  }
 }
