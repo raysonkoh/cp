cb5e4480e5f6e049bd5221bfdc64f14262627202
raysonkoh <rayson.rk.koh@gmail.com>
2021-02-08 05:20:47 +0800

    Upsolve 700-2 D

--- codeforces/700-2/D/m.cc
+++ codeforces/700-2/D/m.cc
@@ -27,8 +27,51 @@ int main() {
   int n;
   cin >> n;
   vector<int> v(n);
-  for (int i = 0; i < n; i++)
+  vector<deque<int>> nxt(n + 1, deque<int>());
+  for (int i = 0; i < n; i++) {
     cin >> v[i];
+    nxt[v[i]].push_back(i);
+  }
 
-  cout << max(dp[m - 1][0], dp[m - 1][1]) << '\n';
+  for (int i = 0; i < n; i++) {
+    nxt[v[i]].push_back(n + 1);
+  }
+
+  vector<int> v1;
+  v1.push_back(0);
+  vector<int> v2;
+  v2.push_back(0);
+  int res = 0;
+  for (int i = 0; i < n; i++) {
+    int x = v[i];
+    if (v1.back() == x) {
+      if (v2.back() != x)
+        res++;
+      v2.push_back(x);
+    } else if (v2.back() == x) {
+      if (v1.back() != x)
+        res++;
+      v1.push_back(x);
+    } else {
+      if (nxt[v1.back()].size() == 0) {
+        if (v2.back() != x)
+          res++;
+        v2.push_back(x);
+      } else if (nxt[v2.back()].size() == 0) {
+        if (v1.back() != x)
+          res++;
+        v1.push_back(x);
+      } else if (nxt[v1.back()].front() < nxt[v2.back()].front()) {
+        v1.push_back(x);
+        res++;
+      } else {
+        v2.push_back(x);
+        res++;
+      }
+    }
+
+    nxt[v[i]].pop_front();
+  }
+
+  cout << res << '\n';
 }
