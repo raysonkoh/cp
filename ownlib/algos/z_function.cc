bc3c64351ef8ca18fddc44bae5352866af778a29
raysonkoh <rayson.rk.koh@gmail.com>
2023-01-24 19:06:49 +0800

    Update zfunc

--- ownlib/algos/z_function.cc
+++ ownlib/algos/z_function.cc
@@ -5,16 +5,24 @@ using namespace std;
 
 // z[i]: max number of characters starting from s_i that matches prefix
 // https://cp-algorithms.com/string/z-function.html
-vector<int> z_function(string s) {
-  int n = (int)s.length();
-  vector<int> z(n);
-  for (int i = 1, l = 0, r = 0; i < n; ++i) {
-    if (i <= r)
-      z[i] = min(r - i + 1, z[i - l]);
-    while (i + z[i] < n && s[z[i]] == s[i + z[i]])
-      ++z[i];
-    if (i + z[i] - 1 > r)
-      l = i, r = i + z[i] - 1;
+class Zf {
+public:
+  vector<int> z;
+  int n;
+
+  Zf(int sz) {
+    n = sz;
+    z.resize(n);
   }
-  return z;
-}
+
+  void run(string &s) {
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+      if (i <= r)
+        z[i] = min(r - i + 1, z[i - l]);
+      while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+        ++z[i];
+      if (i + z[i] - 1 > r)
+        l = i, r = i + z[i] - 1;
+    }
+  }
+};
